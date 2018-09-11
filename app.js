e2e109b59c1155683de7870736c7ef088635fc76
Leandro Schuab <lschuab@gmail.com>
2018-09-11 11:32:36 -0700

    all challenges completed

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 var fs = require('fs');
+
+
+// Challenge 1
+//console.log(fs.readFileSync("./challenge1/info.txt", "utf8"));
+
+// Challenge 2
+// let fileText2 = fs.readFileSync("./challenge2/info.txt", "utf8");
+// fileText2 += "Leandro";
+// fs.writeFileSync("./challenge2/info.txt", fileText2);
+
+// Challenge 3
+// fs.renameSync("./challenge3/binfo.txt", "./challenge3/info.txt");
+
+// Challenge 4
+// fs.mkdirSync("./challenge4/copyfolder/");
+// fs.copyFileSync("./challenge4/info.txt", "./challenge4/copyfolder/info.txt");
+
+// Challenge 5
+// let fileText5 = fs.readFileSync("./challenge5/info.txt", "utf8");
+// fileText5 = fileText5.split("-").join(" ");
+// fs.writeFileSync("./challenge5/info.txt", fileText5);
+
+// Challenge 6
+// const contents = fs.readdirSync("./challenge6");
+// for (const file of contents) {
+//   if (file.includes('.txt')) {
+//     console.log(fs.readFileSync(`./challenge6/${file}`, "utf8"));
+//   }
+// }
